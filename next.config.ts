8afa6990bc643e72d5e0dbc7a79d2661fc8cd25e
Velvet <velvet.pinkish@gmail.com>
2026-02-12 14:20:20 +0100

    Add dynamic page routing

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns: [
-      // {
-      //   protocol: "https",
-      //   hostname: "placeimg.com",
-      //   pathname: "/**"
-      // },
-      // { protocol: "https", 
-      //   hostname: "i.imgur.com", 
-      //   pathname: "/**" 
-      // }
-    ]
   }
 };
 
